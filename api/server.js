f61a6529eb9fbe2c6e93915311ef8c7fb341f43c
ClintFix <clint.fix@gmail.com>
2021-04-06 13:07:49 -0600

    server.js file setup

--- api/server.js
+++ api/server.js
@@ -1,2 +1,20 @@
-// implement your server here
-// require your posts router and connect it here
+//STEP 3: 
+const express = require('express')
+
+// STEP 4: require Routers
+const postsRouter = require('./posts/posts-router')
+
+// STEP 5: set up server
+const server = express();
+
+// STEP 6: Middleware
+server.use(express.json());
+server.use('/api/posts', postsRouter)
+
+// STEP 7: Catch-all endpoint
+server.get('/', (req, res) => {
+    res.send('Welcome to the General API')
+})
+
+// STEP 8: export
+module.exports = server;
