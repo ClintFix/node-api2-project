c98a51ca5b26b446535b43b080dc36a175897fac
ClintFix <clint.fix@gmail.com>
2021-04-06 13:23:18 -0600

    first endpoint done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,30 @@
-// implement your posts router here
+// STEP 9: Requires
+const Posts = require('./posts-model') //import posts model
+const express = require('express');
+const router = express.Router();
+
+// STEP 10: Endpoints
+
+// [GET] - /api/posts - return array of all post objects
+router.get('/', (req, res) => {
+    Posts.find()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(error => {
+            res.status(500).json({message: "The posts information could not be retrieved"})
+        })
+})
+
+// [GET] - /api/posts/:id - return post object with ID
+
+// [POST] - /api/posts - Create new post and return the newly craeted post object
+
+// [PUT] - /api/posts/:id - update post with specific id. return modified document
+
+// [DELETE] - /api/posts/:id - remove post with ID and return deleted post object
+
+// [GET] - /api/posts/:id/comments - return array of all comment ojbects with post with given ID
+
+// STEP 9.5 - EXPORT
+module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// require your server and launch it here
+//STEP 1: Require server
 const server = require('./api/server')
 
+//STEP 2: 
 server.listen(4000, () => {
     console.log('Server running on http://localhost:4000')
 });
